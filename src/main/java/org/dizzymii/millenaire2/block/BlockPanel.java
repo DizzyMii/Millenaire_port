906a981b9a526b461910fc4f9144c23fa2e9f53d
DizzyMii <Kadeheglin@gmail.com>
2026-03-17 01:39:15 -0600

    feat: wire block entities for fire pit, locked chest, panel, import table, and banner

--- src/main/java/org/dizzymii/millenaire2/block/BlockPanel.java
+++ src/main/java/org/dizzymii/millenaire2/block/BlockPanel.java
@@ -1,11 +1,51 @@
 package org.dizzymii.millenaire2.block;
 
+import com.mojang.serialization.MapCodec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import org.dizzymii.millenaire2.entity.blockentity.MillPanelBlockEntity;
 
-public class BlockPanel extends Block {
+import javax.annotation.Nullable;
+
+public class BlockPanel extends BaseEntityBlock {
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+    public static final MapCodec<BlockPanel> CODEC = simpleCodec(BlockPanel::new);
+
     public BlockPanel(BlockBehaviour.Properties props) {
         super(props);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
-    // TODO: Implement panel block entity interaction, sign-like display
+
+    @Override
+    protected MapCodec<? extends BaseEntityBlock> codec() { return CODEC; }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState state) { return RenderShape.MODEL; }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new MillPanelBlockEntity(pos, state);
+    }
 }
